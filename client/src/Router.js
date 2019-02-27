4094b8584ff6691e2dfd659003be267ad416c6d0
Chris Gauthier <chrsgauthier5@gmail.com>
2019-02-26 21:09:10 -0500

    login page

--- client/src/Router.js
+++ client/src/Router.js
@@ -3,6 +3,8 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import App from "./App";
 import Login from "./pages/loginPage";
 import SignUp from "./pages/signUpPage";
+import User from "./pages/userPage";
+import Business from "./pages/businessPage";
 
 const Router = () => (
   <BrowserRouter>
@@ -10,6 +12,8 @@ const Router = () => (
       <Route exact path="/" component={App} />
       <Route path="/signUp" component={SignUp} />
       <Route path="/login" component={Login} />
+      <Route path="/user" component={User} />
+      <Route path="/business" component={Business} />
     </Switch>
   </BrowserRouter>
 );
--- client/src/pages/loginPage.js
+++ client/src/pages/loginPage.js
@@ -1,47 +1,81 @@
 import React, { Component } from "react";
 import Nav from "../components/Nav/index";
 import Form from "../components/Form/index";
+import buttons from "../components/Buttons.json";
+import { Link } from "react-router-dom";
 
-export class LoginPage extends Component {
+class signUpPage extends Component {
+  state = {
+    username: "",
+    password: "",
+    buttons
+  };
+
+  baseState = this.state //grabs a 'snapshot' of empty state object
+
+  onSubmit = event => { //we need this to log the form data to our DB
+    event.preventDefault();
+    console.log(event);
+    this.setState(this.baseState) //clears out the fields
+  };
+
+
+  onChange = event => {
+      this.setState ({[event.target.name] : event.target.value});
+      console.log(this.state);
+      console.log(this.baseState)
+  }
+
+
   render() {
-    const buttons = [
-      {
-        id: 1,
-        name: "Home",
-        display: true,
-        link: "/",
-        color: "primary"
-      },
-      {
-        id: 2,
-        name: "Login",
-        display: false,
-        link: "/login",
-        color: "info"
-      },
-      {
-        id: 3,
-        name: "Sign Up",
-        display: true,
-        link: "/signUp",
-        color: "warning"
-      },
-      {
-        id: 4,
-        name: "Logout",
-        display: false,
-        link: "/",
-        color: "danger"
-      }
-    ];
     return (
       <div>
-        <Nav buttons={buttons} />
-        Please Login to continue
-        <Form />
+        <Nav buttons={this.state.buttons} />
+        
+        <div className="container">
+        <h1 className='text-center'>Welcome Back to SaveMySpot</h1>
+          <form onSubmit={this.onSubmit}>
+          <div className="form-group">
+              <label for="firstname">Username</label>
+              <input
+                type="text"
+                className="form-control"
+                name="username"
+                placeholder="Username"
+                value={this.state.username}
+                onChange={this.onChange}
+              />
+            </div>
+            <div className="form-group">
+              <label for="lastname">Password</label>
+              <input
+                type="password"
+                className="form-control"
+                name="password"
+                placeholder="Password"
+                value={this.state.password}
+                onChange={this.onChange}
+              />
+              </div>
+
+            <Link to="/user">  
+            <button type="submit" className="btn btn-primary">
+              Login - (User Route)
+            </button>
+            </Link>
+            
+            <Link to="/business">  
+            <button type="submit" className="btn btn-primary">
+              Login - (Business Route)
+            </button>
+            </Link>
+          </form>
+        </div>
       </div>
     );
   }
 }
 
-export default LoginPage;
+export default signUpPage;
+
+

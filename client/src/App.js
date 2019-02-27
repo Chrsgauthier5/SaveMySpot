fd737d2f166e080d52f380aa16ece36bb8c33ec9
Chris Gauthier <chrsgauthier5@gmail.com>
2019-02-26 19:53:37 -0500

    signup page created

--- client/src/App.js
+++ client/src/App.js
@@ -3,43 +3,21 @@ import { Link } from "react-router-dom";
 import Nav from "./components/Nav/index";
 import { Col, Row, Container } from "./components/Grid/index"; 
 import Jumbotron from "./components/Jumbotron/index";
+import buttons from "./components/Buttons.json"
 
 class App extends Component {
+
+  state = {
+    buttons
+  };
+
   render() {
-    const buttons = [
-      {
-        id: 1,
-        name: "Home",
-        display: false,
-        link: "/",
-        color: "primary"
-      },
-      {
-        id: 2,
-        name: "Login",
-        display: true,
-        link: "/login",
-        color: "info"
-      },
-      {
-        id: 3,
-        name: "Sign Up",
-        display: true,
-        link: "/signUp",
-        color: "warning"
-      },
-      {
-        id: 4,
-        name: "Logout",
-        display: false,
-        link: "/",
-        color: "danger"
-      }
-    ];
+    
+      
     return (
       
       <div>
-      <Nav buttons={buttons}/>
+      <Nav buttons={this.state.buttons}/>
       <Row>
         <Col size="md-6">
         <Jumbotron>

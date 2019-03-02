3fbc6ebf8d054b447d51f2f301b5c963f5dcd9e4
Jeff Bond <jbond2663@yahoo.com>
2019-03-02 11:47:14 -0500

    added waitlist to business side with users.js and adduser.js

---
+++ client/build/precache-manifest.2c1e17765abb2d3703e391be62b0341a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "a068405c7d2047fc8754",
+    "url": "/static/js/main.a068405c.chunk.js"
+  },
+  {
+    "revision": "74de0fba1f07101839b9",
+    "url": "/static/js/2.74de0fba.chunk.js"
+  },
+  {
+    "revision": "a068405c7d2047fc8754",
+    "url": "/static/css/main.53539217.chunk.css"
+  },
+  {
+    "revision": "909a2e775876368ea6ed8052a49b970e",
+    "url": "/index.html"
+  }
+];
--- client/src/App.js
+++ client/src/App.js
@@ -5,12 +5,48 @@ import { Col, Row, Container } from "./components/Grid/index";
 import Jumbotron from "./components/Jumbotron/index";
 import buttons from "./components/Buttons.json"
 
+import Users from './Users';
+import AddUser from './AddUser';
 
 class App extends Component {
 
   state = {
-    buttons
+    buttons,
+
+    users : [
+      { username: 'Ivan Lane', useremail: 'ivan@yahoo.com', id: 1},
+      { username: 'Jane Smith', useremail: 'jane@gmail.com', id: 2},
+      { username: 'Johh Davis', useremail: 'john@gmail.com', id: 3}
+    ]
+
   };
+
+  // function to add new user to array above
+  // parameter is newUser is taken in
+  addUserFn = (newUser) => {
+    console.log(newUser)
+    // give new user a unique id
+    newUser.id = Math.random();
+    // make copy of original array - note spread operator ... equates to the three names above
+    let users = [...this.state.users, newUser] 
+    this.setState({
+      // original state array gets new array assigned to it
+      users: users
+    })
+  }
+
+  // delete user fn
+  deleteUserFn = (id) => {
+    console.log(id)
+    // use filter to remove speific varibale
+    let users = this.state.users.filter(user => {
+      // if true below it will be added to new array
+      return user.id !== id
+    });
+    this.setState({
+      users: users
+    })
+  }
 
   render() {
     
@@ -37,9 +73,15 @@ class App extends Component {
           no longer have to wait in line for your excellent services and your employees can space 
           out their customers better. This is good for:
           Auto Service, barbers, nail salons, etc... 
-
         </p>
-        
+{/* adding userlist display and functiona for business  */}
+            {/* pass the delete org function in below */}
+            {/* pass in the array above below */}
+            <Users deleteUserFn={this.deleteUserFn} users={this.state.users}/>
+            {/* pass in the function above below */}
+            <AddUser addUserFn={this.addUserFn}/>
+
+
         </Jumbotron>
         </Col>
       </Row>

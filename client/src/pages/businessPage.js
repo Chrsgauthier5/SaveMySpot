ee2d6a38b9b47c5ac06a831ea4a7e8c270470ca6
Jeff Bond <jbond2663@yahoo.com>
2019-03-07 19:49:01 -0500

    moved waitlist render from root app.js to /pages/businesspage along with user and adduser files to /pages

--- client/src/pages/businessPage.js
+++ client/src/pages/businessPage.js
@@ -5,13 +5,48 @@ import { Col, Row, Container } from "../components/Grid/index";
 import Jumbotron from "../components/Jumbotron/index";
 import Nav from "../components/Nav/index";
 
+import Users from './Users';
+import AddUser from './AddUser';
+
 class businessPage extends Component {
 
     state = {
-        buttons
-    }
+        buttons,
 
+        users : [
+          { username: 'Ivan Lane', useremail: 'ivan@yahoo.com', id: 1},
+          { username: 'Jane Smith', useremail: 'jane@gmail.com', id: 2},
+          { username: 'Johh Davis', useremail: 'john@gmail.com', id: 3}
+        ]
+    
+    };
 
+  // function to add new user to array above
+  // parameter is newUser is taken in
+  addUserFn = (newUser) => {
+    console.log(newUser)
+    // give new user a unique id
+    newUser.id = Math.random();
+    // make copy of original array - note spread operator ... equates to the three names above
+    let users = [...this.state.users, newUser] 
+    this.setState({
+      // original state array gets new array assigned to it
+      users: users
+    })
+  }
+
+  // delete user fn
+  deleteUserFn = (id) => {
+    console.log(id)
+    // use filter to remove speific varibale
+    let users = this.state.users.filter(user => {
+      // if true below it will be added to new array
+      return user.id !== id
+    });
+    this.setState({
+      users: users
+    })
+  }
 
     render() {
       return (
@@ -21,16 +56,18 @@ class businessPage extends Component {
           <Col size="md-12">
           <Jumbotron>
           <h2> Welcome Haircuts by Chris! Here is a list of your customers:</h2>
-          {/* <p> Jeff and Chris' code here </p> */}
-          {/* <button className="save">Save My Spot</button> */}
-          
+
+            {/* <p> Jeff and Chris' code here </p> */}
+            {/* <button className="save">Save My Spot</button> */}
+            {/* adding userlist display and functiona for business  */}
+            {/* pass the delete org function in below */}
+            {/* pass in the array above below */}
+            <Users deleteUserFn={this.deleteUserFn} users={this.state.users}/>
+            {/* pass in the function above below */}
+            <AddUser addUserFn={this.addUserFn}/>
+         
           </Jumbotron>
           </Col>
-          {/* <Col size="md-6">
-          <Jumbotron>
-          <h1>Data here - open to formatting</h1>
-          </Jumbotron>
-          </Col> */}
         </Row>
         </div>
       )
